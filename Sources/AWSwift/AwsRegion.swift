cc32b1eb51dab6159ecc7f5f465294dd7c806e90
Tim <0xtimc@gmail.com>
2016-11-30 13:32:38 +0000

    Added all the regions for AWS

--- Sources/AWSwift/AwsRegion.swift
+++ Sources/AWSwift/AwsRegion.swift
@@ -1,3 +1,14 @@
 public enum AwsRegion: String {
-    case euWest1 = "eu-west-1"
+    case usEastNVirginia = "us-east-1"
+    case usWestOregon = "us-west-2"
+    case usWestNCalifornia = "us-west-1"
+    case euIreland = "eu-west-1"
+    case euFrankfurt = "eu-central-1"
+    case asiaSingapore = "ap-southeast-1"
+    case asiaTokyo = "ap-northeast-1"
+    case asiaSeoul = "ap-northeast-2"
+    case asiaSydney = "ap-southeast-2"
+    case southAmerica = "sa-east-1"
+    case awsGovCloud = "us-gov-west-1"
+    case china = "cn-north-1"
 }
